c9ffb6f7162b6fee8d9737f8b67167e4a5dc55a3
TactileFox <dmp2024@yahoo.com>
2024-12-18 21:24:49 -0600

    Tuple args -> reg args

--- psql_connection.py
+++ psql_connection.py
@@ -28,31 +28,31 @@ async def create_message_log(message: Message) -> None:
 # Check, Update, Insert
 async def log_user(conn: Connection, user: User) -> None:
 
-    row = await conn.fetchrow(get_user_query(), (user.id,))
+    row = await conn.fetchrow(get_user_query(), user.id)
 
     if not row:
         try:
-            await conn.execute(insert_user_query(), (user.id, user.name, get_date()))
+            await conn.execute(insert_user_query(), user.id, user.name, get_date())
         except Exception as e:
             print(f'Error creating user {user.name} with exception {e}')
     elif row['Username'] != user.name:
         try:
-            await conn.execute(update_user_query(), (user.name, datetime.now(timezone.utc), user.id))
+            await conn.execute(update_user_query(), user.name, datetime.now(timezone.utc), user.id)
         except Exception as e:
             print(f'Error updating user {user.name} with exception {e}')
 
 async def log_guild(conn: Connection, guild: Guild) -> None:
 
-    row = await conn.fetchrow(get_guild_query(), (guild.id,))
+    row = await conn.fetchrow(get_guild_query(), guild.id)
 
     if not row:
         try:
-            await conn.execute(insert_quild_query(), (guild.id, guild.name, guild.description, get_date()))
+            await conn.execute(insert_quild_query(), guild.id, guild.name, guild.description, get_date())
         except Exception as e:
             print(f'Error inserting guild {guild.name} with exception {e}')
     elif row['Name'] != guild.name or row['Description'] != guild.description:
         try:
-            await conn.execute(update_guild_query(), (guild.name, guild.description, get_date(), guild.id))
+            await conn.execute(update_guild_query(), guild.name, guild.description, get_date(), guild.id)
         except Exception as e:
             print(f'Error updating guild {guild.name} with exception {e}')
 
@@ -60,64 +60,63 @@ async def log_channel(conn: Connection, message: Message) -> None:
 
     channel = message.channel
 
-    row = await conn.fetchrow(get_channel_query(), (channel.id,))
+    row = await conn.fetchrow(get_channel_query(), channel.id)
 
     # Update channel.name property
     channel_name = await create_channel_name(message)
 
     if not row:
         try:
-            await conn.execute(insert_channel_query(), (channel.id, channel.type.value, channel_name, channel.guild.id, channel.category.name, get_date()))
+            await conn.execute(insert_channel_query(), channel.id, channel.type.value, channel_name, channel.guild.id, channel.category.name, get_date())
         except Exception as e:
             print(f'Error inserting channel {channel_name} with exception {e}')
     elif row['Name'] != channel.name or row['ChannelTypeId'] != channel.type.value:
         try:
-            await conn.execute(update_channel_query(), (channel.name, channel.type.value, channel.id))
+            await conn.execute(update_channel_query(), channel.name, channel.type.value, channel.id)
         except Exception as e:
             print(f'Error updating channel {channel.name} with exception {e}')
 
 async def log_message(conn: Connection, message: Message) -> None:
 
-    row = await conn.fetchrow(get_message_query(), (message.id,))
+    row = await conn.fetchrow(get_message_query(), message.id)
 
     if not row:
         try:
-            await conn.execute(insert_message_query(), (message.id, message.author.id, message.guild.id, message.content, message.channel.id, get_date()))
+            await conn.execute(insert_message_query(), message.id, message.author.id, message.guild.id, message.content, message.channel.id, get_date())
         except Exception as e:
             print(f'Error inserting message {message.content[:20]}... with exception {e}')
 
 async def log_attachment(conn: Connection, attachment: Attachment, message_id):
 
-    row = await conn.fetchrow(get_attachment_query(), (attachment.id,))
+    row = await conn.fetchrow(get_attachment_query(), attachment.id)
     
     if not row:
         try:
-            await conn.execute(insert_attachment_query(), (attachment.id, attachment.filename, attachment.content_type, attachment.url, message_id, get_date()))
+            await conn.execute(insert_attachment_query(), attachment.id, attachment.filename, attachment.content_type, attachment.url, message_id, get_date())
             print(f'Attachment {attachment.filename[:20]} added successfully')
         except Exception as e:
             print(f'Error inserting attachment {attachment.filename[:20]} with exception {e}')
     elif row['Filename'] != attachment.filename or row['ContentType'] != attachment.content_type or row['URL'] != attachment.url or row['MessageId'] != message_id:
         return NotImplementedError('Attachment already exists')
     
-    cur.close()
+
 async def log_user_mention(conn: Connection, user: User, message_id, message_sender_id) -> None:
 
     # TODO Don't log bot mentions
     # if message_sender_id == bot.id: return
 
-    row = await conn.fetchrow(get_user_mentions_query(), (message_id, message_sender_id))
+    row = await conn.fetchrow(get_user_mentions_query(), message_id, message_sender_id)
 
     if not row: 
         try:
-            await conn.execute(insert_user_mentions_query(), (message_id, message_sender_id, user.id, get_date()))
+            await conn.execute(insert_user_mentions_query(), message_id, message_sender_id, user.id, get_date())
         except Exception as e:
             print(f'Error inserting mention {user.name} with exception {e}')
 
-    cur.close()
 async def get_message_counts(guild: Guild) -> File:
     
     conn = await get_db_connection()
-    data = await conn.fetchmany('SELECT COUNT("M"."Id"), "U"."Username" FROM "Message" "M" INNER JOIN "User" "U" ON "U"."Id" = "M"."UserId" WHERE "M"."GuildId" = $1 GROUP BY "U"."Username" ORDER BY COUNT("M"."Id") DESC LIMIT 10', (guild.id,))
+    data = await conn.fetch('SELECT COUNT("M"."Id"), "U"."Username" FROM "Message" "M" INNER JOIN "User" "U" ON "U"."Id" = "M"."UserId" WHERE "M"."GuildId" = $1 GROUP BY "U"."Username" ORDER BY COUNT("M"."Id") DESC LIMIT 10', guild.id)
 
     # Create matplotlib graph
     fig, ax = plt.subplots()
@@ -132,7 +131,7 @@ async def get_message_counts(guild: Guild) -> File:
     buf.seek(0)
 
     # Create and return file 
-    file = File(buf, 'message_count_graph.png') # File stored in buf, title
+    file = File(buf, 'message_count_graph.png')
     plt.close()
     buf.close()
     await conn.close()
@@ -143,7 +142,7 @@ async def log_message_edit(before: Message, after: Message) -> None:
     
     conn = await get_db_connection()
 
-    row = await conn.fetchmany('SELECT "Id" FROM "Message" WHERE "Id" = $1', (before.id,)) # Check if before message exists
+    row = await conn.fetchrow('SELECT "Id" FROM "Message" WHERE "Id" = $1', before.id) # Check if before message exists
 
     if not row: 
         await create_message_log(before)
@@ -152,7 +151,7 @@ async def log_message_edit(before: Message, after: Message) -> None:
         for attachment in before.attachments:
             if attachment not in after.attachments:
                 # Delete record
-                await conn.execute(delete_attachment_query(), (get_date(), get_date(), attachment.url, before.id)) 
+                await conn.execute(delete_attachment_query(), get_date(), get_date(), attachment.url, before.id) 
         # Log new attachments
         for attachment in after.attachments:
             if attachment not in before.attachments:
@@ -161,16 +160,16 @@ async def log_message_edit(before: Message, after: Message) -> None:
         for mention in before.mentions:
             if mention not in after.mentions:
                 # Delete record
-                await conn.execute(delete_user_mention_query(), (get_date(), get_date(), before.id, mention.id))
+                await conn.execute(delete_user_mention_query(), get_date(), get_date(), before.id, mention.id)
         for mention in after.mentions:
             if mention not in before.mentions:
                 # Insert record
                 await log_user_mention(conn, mention, before.id, before.author.id)
-        await conn.execute(update_message_query(), (after.content, 1, 0, get_date(), None, after.id))
+        await conn.execute(update_message_query(), after.content, 1, 0, get_date(), None, after.id)
     except Exception as e:
         print(f'Error updating message with exception {e}')
     try:
-        await conn.execute(insert_message_edit_query(), (before.id, before.content, after.content, get_date()))
+        await conn.execute(insert_message_edit_query(), before.id, before.content, after.content, get_date())
     except Exception as e:
         print(f'Error inserting message edit with exception {e}')
 
@@ -179,12 +178,12 @@ async def log_message_deletion(message: Message) -> None:
 
     conn = await get_db_connection()
 
-    row = await conn.fetchrow('SELECT "Id" FROM "Message" WHERE "Id" = %s', (message.id,))
+    row = await conn.fetchrow('SELECT "Id" FROM "Message" WHERE "Id" = $1', message.id)
 
     if not row:
         await create_message_log(message)
     try: 
-        await conn.execute("""UPDATE "Message" SET "DeleteDateUTC" = %s, "Deleted" = %s, "UpdateDateUTC" = %s WHERE "Id" = %s""", (get_date(), 1, get_date(), message.id))
+        await conn.execute("""UPDATE "Message" SET "DeleteDateUTC" = $2, "Deleted" = $3, "UpdateDateUTC" = $4 WHERE "Id" = $5""", get_date(), 1, get_date(), message.id)
     except Exception as e:
         print(f'Error updating message {message.id} with exception {e}')
     
@@ -200,7 +199,7 @@ async def log_message_reaction(reaction: Reaction, user: User) -> None:
     try:
         emoji_name = reaction.emoji.name if type(reaction.emoji) != str else reaction.emoji
         # TODO put insert reaction query into a new function.
-        await conn.execute(insert_reaction_query(), (reaction.message.id, user.id, emoji_name, self_react, get_date()))
+        await conn.execute(insert_reaction_query(), reaction.message.id, user.id, emoji_name, self_react, get_date())
     except Exception as e:
         print(f'Error inserting reaction {emoji_name} with exception {e}')
 
@@ -211,7 +210,7 @@ async def log_reaction_deletion(reaction: Reaction, user: User) -> None:
 
     try:
         emoji_name = reaction.emoji.name if type(reaction.emoji) != str else reaction.emoji
-        await conn.execute(delete_reaction_query(), (get_date(), reaction.message.id, user.id, emoji_name))
+        await conn.execute(delete_reaction_query(), get_date(), reaction.message.id, user.id, emoji_name)
     except Exception as e:
         print(f'Error updating reaction {emoji_name} with exception {e}')
 
@@ -226,7 +225,7 @@ async def log_reaction_clear(reactions: list[Reaction], message: Message) -> Non
     else:
         for reaction in reactions:
             try:
-                await conn.execute('UPDATE "Reactions" SET "DeleteDateUTC" = $1, "Deleted" = 1 WHERE "MessageId" = $2 AND "Deleted" = 0', (get_date(), message.id))
+                await conn.execute('UPDATE "Reactions" SET "DeleteDateUTC" = $1, "Deleted" = 1 WHERE "MessageId" = $2 AND "Deleted" = 0', get_date(), message.id)
             except Exception as e:
                 emoji_name = reaction.emoji.name if type(reaction.emoji) != str else reaction.emoji 
                 print(f'Error deleting reaction {emoji_name} with exception {e}')
@@ -237,13 +236,13 @@ async def get_last_updated_message(channel_id) -> tuple:
 
     conn = await get_db_connection()
 
-    row = await conn.fetchrow('SELECT "M"."Id" AS "MessageId", "M"."Content", "U"."Username", "M"."Deleted" FROM "Message" "M" INNER JOIN "User" "U" ON "U"."Id" = "M"."UserId" WHERE "M"."ChannelId" = $1 AND "M"."UpdateDateUTC" IS NOT NULL ORDER BY "M"."UpdateDateUTC" DESC LIMIT 1', (channel_id,))
+    row = await conn.fetchrow('SELECT "M"."Id" AS "MessageId", "M"."Content", "U"."Username", "M"."Deleted" FROM "Message" "M" INNER JOIN "User" "U" ON "U"."Id" = "M"."UserId" WHERE "M"."ChannelId" = $1 AND "M"."UpdateDateUTC" IS NOT NULL ORDER BY "M"."UpdateDateUTC" DESC LIMIT 1', channel_id)
 
     if not row:
         return (None,)
     if row['Deleted'] == 0:
         username = row['Username']
-        row = await conn.fetchrow('SELECT "BeforeContent", "AfterContent" FROM "MessageEditHistory" WHERE "MessageId" = $1 ORDER BY "CreateDateUTC" DESC LIMIT 1', (row['MessageId'],))
+        row = await conn.fetchrow('SELECT "BeforeContent", "AfterContent" FROM "MessageEditHistory" WHERE "MessageId" = $1 ORDER BY "CreateDateUTC" DESC LIMIT 1', row['MessageId'])
         await conn.close()
         return (row['MessageId'], row['Content'], username, 'edited')
     else:
