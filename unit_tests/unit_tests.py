ba8b050e3b6d57a873f6080e164f80ae822c8f2a
TactileFox <dmp2024@yahoo.com>
2024-12-20 22:29:24 -0600

    Added initial tests

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -10,9 +10,7 @@ from unittest.mock import patch, MagicMock
 # In terminal, run python -m unittest unit_tests.py
     
 
-
-
-class TestAPI(unittest.TestCase):
+class TestAPI(unittest.IsolatedAsyncioTestCase):
 
     @classmethod # Runs once at start
     def setUpClass(cls):
@@ -25,22 +23,12 @@ class TestAPI(unittest.TestCase):
             cls.mock_nasa_apod_singular_json = json.load(f)
         with open('./unit_tests/mock_nasa_apod_range.json') as f:
             cls.mock_nasa_apod_range_json = json.load(f)
+        
 
-    @classmethod # Runs once at end
-    def tearDownClass(cls):
-        return NotImplemented
-
-    def setUp(self): # runs before every test method
-        pass
-    
-    def tearDown(self): # runs after every test method
-        pass 
-
-
-    def test_weather_api(self):
+    async def test_weather_api(self):
         #TODO what is a context manager??
         with patch('requests.get') as mocker:
-            
+
             # Points
             mock_response_one = MagicMock()
             mock_response_one.status_code = 200
@@ -52,34 +40,99 @@ class TestAPI(unittest.TestCase):
             mock_response_two.json.return_value = self.mock_nws_gridpoints_json
             
             mocker.side_effect = [mock_response_one, mock_response_two]
-            result_city, result_state, result_list = get_usa_weather(46.0, 76.0, 'F')
+            result_city, result_state, result_list = await get_usa_weather(46.0, 76.0, 'F')
 
             self.assertEqual(result_city, 'Rochester')
             self.assertEqual(result_state, 'MI')
             self.assertEqual(result_list, self.mock_nws_gridpoints_json['properties']['periods'])
-
+        
             # Invalid Points
 
 
             # Status 400
 
-    def test_apod_api(self):
+    async def test_apod_api_singular_200(self):
 
         # Singular, 200
-        mock_response_singular_200 = MagicMock()
-        mock_response_singular_200.status_code = 200
-        mock_response_singular_200.json.return_value = self.mock_nasa_apod_singular_json
-        result_urls, result_dates, result_titles, result_explanations = get_astronomy_picture()
-        self.assertEqual(result_urls, self.mock_nasa_apod_singular_json['hdurl'])
-        self.assertEqual(result_dates, self.mock_nasa_apod_singular_json['date'])
-        self.assertEqual(result_titles, self.mock_nasa_apod_singular_json['title'])
-        self.assertEqual(result_explanations, self.mock_nasa_apod_singular_json['explanation'])
+        with patch('requests.get') as mocker:
+            mock_response_singular_200 = MagicMock()
+            mock_response_singular_200.status_code = 200
+            mock_response_singular_200.json.return_value = self.mock_nasa_apod_singular_json
 
-        # Range, 200
-        mock_response_range_200 = MagicMock()
-        mock_response_range_200.status_code = 200
+            mocker.return_value = mock_response_singular_200
 
-        
+            result_urls, result_dates, result_titles, result_explanations = await get_astronomy_picture()
+            self.assertEqual(result_urls[0], self.mock_nasa_apod_singular_json['hdurl'])
+            self.assertEqual(result_dates[0], self.mock_nasa_apod_singular_json['date'])
+            self.assertEqual(result_titles[0], self.mock_nasa_apod_singular_json['title'])
+            self.assertEqual(result_explanations[0], self.mock_nasa_apod_singular_json['explanation'])
+    
+    async def test_apod_range_200(self):
+        with patch('requests.get') as mocker:
+            # Range, 200
+            mock_response_range_200 = MagicMock()
+            mock_response_range_200.status_code = 200
+            mock_response_range_200.json.return_value = self.mock_nasa_apod_range_json
 
-urls, dates, titles, explanations
+            mocker.return_value = mock_response_range_200
+
+            urls: list[str] = list(); dates: list[str] = list(); titles: list[str] = list(); explanations: list[str] = list()
+            for point in self.mock_nasa_apod_range_json:
+                urls.append(point['hdurl'])
+                dates.append(point['date'])
+                titles.append(point['title'])
+                explanations.append(point['explanation'])
+
+            result_urls, result_dates, result_titles, result_explanations = await get_astronomy_picture('2024-12-05', '2024-12-20')
+            self.assertEqual(result_urls, urls)
+            self.assertEqual(result_dates, dates)
+            self.assertEqual(result_titles, titles)
+            self.assertEqual(result_explanations, explanations)
+    
+    async def test_apod_400(self):
+        with patch('requests.get') as mocker:
+            # 400
+            mock_response_400 = MagicMock()
+            mock_response_400.status_code = 400
+
+            mocker.return_value = mock_response_400
+
+            self.assertIsNone(await get_astronomy_picture())
+
+    async def test_apod_empty_list_200(self):
+        with patch('requests.get') as mocker:
+            # Empty List, 200
+            mock_response_empty_one = MagicMock()
+            mock_response_empty_one.status_code = 200
+            mock_response_empty_one.json.return_value = list()
+
+            mock_response_empty_two = MagicMock()
+            mock_response_empty_two.status_code = 200
+            mock_response_empty_two.json.return_value = self.mock_nasa_apod_singular_json
+
+            mocker.side_effect = [mock_response_empty_one, mock_response_empty_two]
+
+            result_urls, result_dates, result_titles, result_explanations = await get_astronomy_picture()
+            self.assertEqual(result_urls[0], self.mock_nasa_apod_singular_json['hdurl'])
+            self.assertEqual(result_dates[0], self.mock_nasa_apod_singular_json['date'])
+            self.assertEqual(result_titles[0], self.mock_nasa_apod_singular_json['title'])
+            self.assertEqual(result_explanations[0], self.mock_nasa_apod_singular_json['explanation'])
+
+            self.assertEqual(mocker.call_count, 2)
+
+    async def test_apod_empty_list_400(self):
+        with patch('requests.get') as mocker:
+            # Empty List, 200
+            mock_response_empty_one = MagicMock()
+            mock_response_empty_one.status_code = 200
+            mock_response_empty_one.json.return_value = list()
+
+            # 400
+            mock_response_empty_two = MagicMock()
+            mock_response_empty_two.status_code = 400
+
+            mocker.side_effect = [mock_response_empty_one, mock_response_empty_two]
+
+            self.assertIsNone(await get_astronomy_picture())
+            self.assertEqual(mocker.call_count, 2)
     
