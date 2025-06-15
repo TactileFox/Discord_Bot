d2250a8586408a062fae2071e5051bb202e978b4
TactileFox <dmp2024@yahoo.com>
2025-06-15 12:41:02 -0500

    Create main.py

--- back_end/app/main.py
+++ back_end/app/main.py
@@ -1,5 +1,18 @@
+import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.database.database import connect_to_db, disconnect_db
+from database.database import connect_to_db, disconnect_db
+from api import guild_controller, message_controller, user_controller
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connect_to_db(os.getenv('CONNECTION_STRING'))
+    yield
+    await disconnect_db()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(guild_controller.router)
+app.include_router(message_controller.router)
+app.include_router(user_controller.router)
