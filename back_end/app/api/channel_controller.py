399fb4730f31ed287f6a86588ce6690238e80591
TactileFox <dmp2024@yahoo.com>
2025-06-30 16:51:21 -0500

    Create channel controller

---
+++ back_end/app/api/channel_controller.py
@@ -0,0 +1,17 @@
+import services.channel_service as channel_service
+from fastapi import APIRouter, HTTPException
+from database.database import acquire_connection
+from models.channel import Channel
+
+router = APIRouter(prefix='/channels')
+
+
+@router.get('/{id}', response_model=Channel)
+async def get_channel_by_id(
+    id: int
+) -> Channel:
+    async with acquire_connection() as conn:
+        message = await channel_service.get_by_id(conn, id)
+        if not message:
+            raise HTTPException(status_code=404, detail='Channel not found')
+        return message
